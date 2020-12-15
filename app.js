3ef4fdde361b176543e2fb443bf7cf5c19433299
Abdifatah Basher <56160528+abasher423@users.noreply.github.com>
2020-12-15 04:18:16 +0000

    mole now hops from a random hole

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
-const moles = document.querySelectorAll('.mole')
-const startBtn = document.querySelector('.btn')
+// add levels (expert mode) [DONE]
+// save to local storage (add a scoreboard)
+// add sound effects (OPTIONAL)
+// add animation to when mole is clicked
+// make moles stay down
 
-const rand = Math.floor(Math.random() * 130)
-const molesArr = []
-const PlayGame = () => {
-    const randomMole = Math.floor(Math.random()*moles.length)
-    for (let i=0; i<=randomMole; i++){
-        moles[i].classList.toggle('moleanimation')
-    }
+const moles = document.querySelectorAll('.mole')
+const holes = document.querySelectorAll('.hole')
+const scoreboard = document.querySelector('.score')
+const startBtn = document.querySelector('.start')
+const dropdown = document.querySelector('select')
+const status = document.querySelector('.status')
+const roundLabel = document.querySelector('.round')
+const resetBtn = document.querySelector('.reset')
+
+let lastHole
+let timeUp = false
+let score = 0
+let currentScore = 0;
+let isPlaying = false
+// let activeMoles = []
+let difficulty = ''
+let time = 0
+let round = ''
+status.innerText = '"Only those who dare to fail greatly can ever achieve greatly"'
+
+
+function randomTime(min, max){
+    return Math.floor(Math.random() * (max - min) + min)
 }
 
-startBtn.addEventListener('click', PlayGame)
+function randomHole(holes){
+    const idx = Math.floor(Math.random() * holes.length)
+    const hole = holes[idx]
+    if(hole === lastHole){ // so mole doesn't pop from the same hole
+        return randomHole(holes)
+    }
+    lastHole = hole
+    return hole
+}
